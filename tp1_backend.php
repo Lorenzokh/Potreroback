d3a37de54ba5660f0e497dd0d20e8c89337798f0
Lorenzokh <113941205+Lorenzokh@users.noreply.github.com>
2023-09-12 14:54:02 -0300

    Add files via upload

---
+++ tp1_backend.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<h1>Primer php</h1>
+<?php
+echo "HOLA MUNDO" ;
+
+$saludo = "Hola Mundo" ;
+echo $saludo ;
+
+
+
+$valor1 = 20 ;
+$valor2 = 5 ;
+echo  $valor1+$valor2 ;
+
+echo  $valor1-$valor2 ;
+echo  $valor1*$valor2 ;
+echo  $valor1/$valor2 ;
+
+$grados = 20 ;
+$n1 = 9 ;
+$n2 = 5 ;
+$n3 = 32 ;
+echo $grados*$n1 ;
+
+$lado1 = 18 ;
+$lado2 = 12 ;
+echo $lado1+$lado2+$lado1+$lado2 ;
+echo $lado1*$lado2 ;
+
+$radio = 30 ;
+$pi = 314 ;
+echo $radio*$radio*$pi ;
+echo $radio*2*$pi ;
+
+ ?>
+  </body>
+</html>
