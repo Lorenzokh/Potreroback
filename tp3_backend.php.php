fd71e39cf28f0ad00dc072af99345d44a8a7bed8
Lorenzokh <113941205+Lorenzokh@users.noreply.github.com>
2023-10-03 17:53:06 -0300

    Add files via upload

---
+++ tp3_backend.php.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+
+print "Ejercicio 1";
+
+for ($i=1; $i <=100 ; $i++) {
+print "<p> $i</p>\n";
+}
+echo "<br>";
+
+print "Ejercicio 2";
+for ($i=100; $i >=1 ; $i--) {
+print "<p> $i</p>\n";
+}
+echo "<br>";
+
+print "Ejercicio 3";
+for ($i=2; $i <100 ; $i+=2) {
+print "<p> $i</p>\n";
+}
+echo "<br>";
+
+print "Ejercicio 4";
+for ($i=1; $i <=100 ; $i+=2) {
+print "<p> $i</p>\n";
+}
+echo "<br>";
+
+print "No pude con la sumatoria";
+
+
+
+
+
+
+
+
+print "<p>FINAL</p>\n";
+
+     ?>
+  </body>
+</html>
